a5eaebe3c999486756f2dd9d089f07dd931f387c
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-04-11 20:29:40 -0300

    Atualização de progresso

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-import App from "./App.css";
-import ShoppingList from "./components/ShoppingList";
-import InputItem from "./components/InputItem";
+import "./App.css";
 
-export default() => (
+import ShoppingList from "./components/ShoppingList";
+
+
+export default () => (
     <>
         <h1>Lista de compras</h1>
+        
         <ShoppingList></ShoppingList>
-        <InputItem></InputItem>
     </>
 
 )
--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,31 +1,58 @@
-import React from "react";
+import { useState } from "react";
 
-import ShoppingList from "./ShoppingList.css"
+import './ShoppingList.css'
 
-export default () => (
-    <>
-        <div className="list-itens">
+export default function ShoppingList() { 
+
+    const [itemsList, setItemsList] = useState([
+          {id: 1, item:'teste'},
+          {id: 2, item:'teste2'}
+        ])
+    
+    function createItem() {
+      return (itemsList.map(item => {
+        return (
+          <>
+          <li id={item.id} className="items">
+            <div className="name-item">
+              <label>{item.item}</label>
+            </div>
+            <div className="div-icons">
+              <i>V</i>
+              <i>X</i>
+            </div>
+          </li>
+          </>
+        );
+      }));
+    }
+
+    function inputItem() {
+        return (
+            <>
+                <div className="new-item">
+                    <input id="save-item" type="text" placeholder="Novo item..." />
+                    <div className="button-add">
+                        <i onClick={save}>+</i>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    function save() {   
+        const newItem = document.getElementById('save-item').value
             
-            <div className="itens">
-                <div className="name-item">
-                    <label>Item_1</label>
-                </div>
-                <div className="div-icons">
-                    <i>V</i>
-                    <i>X</i>
-                </div>
-            </div>
-
-            <div className="itens">
-                <div className="name-item">
-                    <label>Item_2</label>
-                </div>
-                <div className="div-icons">
-                    <i>V</i>
-                    <i>X</i>
-                </div>
-            </div>
-
-        </div>
-    </>
-)
+        setItemsList([
+            ...itemsList,
+            {id: 3, item: newItem}]
+            )     
+    }
+    
+    return (
+      <>
+        <ul>{createItem()}</ul>
+        {inputItem()}
+      </>
+    ) 
+  }
