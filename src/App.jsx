e2f2524bae2a276e95fdc228159f17bc6d19e11b
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-04-13 14:20:25 -0300

    atualizações

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import "./App.css";
 import ShoppingList from "./components/ShoppingList";
 
 
-export default () => (
-    <>
-        <h1>Lista de compras</h1>
-        
-        <ShoppingList></ShoppingList>
-    </>
+export default function App() {
+    return (
+        <>
+            <h1>Lista de compras</h1>
 
-)
+            <ShoppingList></ShoppingList>
+        </>
+    )
+}
