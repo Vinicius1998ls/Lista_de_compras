ca7b286e3b6add8ac0e2bdda00b639d504787d86
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-04-23 20:24:35 -0300

    Comentarios adicionados

--- src/components/ItemName.jsx
+++ src/components/ItemName.jsx
@@ -1,10 +1,13 @@
 import './ItemName.css'
 
 export default function itemName(props) {
-    
+    // se false carrega no do item normal
+    // se true carrega o item com um risco
     if(props.check === false) {        
         return (
             <>
+                {/* se for menor que 16 caracteres carrega com a fonte normal
+                se for maior carrega com uma fonte menor */}
                 {props.name.length < 16 ? <label className='normal-name'>{props.name}</label> : 
                 <label className="big-name">{props.name}</label>}                
             </>
--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -5,46 +5,57 @@ import ItemName from "./ItemName";
 import './ShoppingList.css'
 import './ClearButton.css'
 
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faXmark, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export default function ShoppingList() { 
+export default function ShoppingList() {
 
+    //Criação do estado da lista
     const [itemsList, setItemsList] = useState([])
+    //Estado do botão de adicionar
     const [enable, setEnable] = useState(false)
-    
+
+    //Renderiza os itens da lista
     function createItem() {
-      return (itemsList.map(item => {
-        return (          
-          <>            
-            <li id={item.id} key={item.id} className="items">
-              <div className="name-item">
-                <ItemName name={item.item} check={item.checked} ></ItemName>             
-              </div>
-              <div className="div-icons">
-                {item.checked ? 
-                    <FontAwesomeIcon icon={faXmark} className="remove" onClick={() => remove(item.id)} /> : 
+        //renderiza a lista
+        return (itemsList.map(item => {
+            return (
                 <>
-                    <FontAwesomeIcon icon={faCheck} className="check" onClick={() => check(item.id)} />
-                    <FontAwesomeIcon icon={faXmark} className="remove" onClick={() => remove(item.id)} />
-                    
-                </>}
-              </div>
-            </li>
-          </>
-        );
-      }));
+                    <li id={item.id} key={item.id} className="items">
+                        <div className="name-item">
+                            <ItemName name={item.item} check={item.checked} ></ItemName>
+                        </div>
+                        <div className="div-icons">
+                            {/* se item estiver marcado como confirmado ele renderiza só o botão cancelar
+                            se não ele renderiza os botões confirmar e cancelar */}
+                            {item.checked ?
+                                <FontAwesomeIcon icon={faXmark} className="remove" onClick={() => remove(item.id)} /> :
+                                <>
+                                    <FontAwesomeIcon icon={faCheck} className="check" onClick={() => check(item.id)} />
+                                    <FontAwesomeIcon icon={faXmark} className="remove" onClick={() => remove(item.id)} />
+                                </>}
+                        </div>
+                    </li>
+                </>
+            );
+        }));
     }
 
+    //Cria o input para inserir o novo item
     function inputItem() {
         return (
             <>
-                <div className="new-item">                    
-                    <input id="save-item" type="text" maxLength={22} 
-                    placeholder="Novo item..." onKeyDown={enter} onInput={handleChangeEnable} />
+                <div className="new-item">
+                    <input id="save-item" type="text" maxLength={22}
+                        // enter adiciona o item                    
+                        placeholder="Novo item..." onKeyDown={enter} onInput={handleChangeEnable} />
                     <div className="button-add">
-                        {enable ? <FontAwesomeIcon icon={faPlus} onClick={save} style={{color: '#CE050F'}} /> :
-                        <FontAwesomeIcon icon={faPlus} style={{color: '#494949'}} />}                        
+                        {/* ao digitar chama a função handleChangeEnable, 
+                        se for verdadeiro o botão adionar é habilitado
+                        se falso desabilita o botão adicionar e torna impossivel adicionar um novo item 
+                        mesmo que aperte enter */}
+                        {enable ? <FontAwesomeIcon icon={faPlus} onClick={save} style={{ color: '#CE050F' }} /> :
+                            <FontAwesomeIcon icon={faPlus} style={{ color: '#494949' }} />}
                     </div>
                 </div>
             </>
@@ -52,82 +63,97 @@ export default function ShoppingList() {
     }
 
     function enter(event) {
-      if(event.keyCode === 13) {
-        save()
-      }
+        // se enter for apertado chama a função save
+        if (event.keyCode === 13) {
+            save()
+        }
     }
 
     function save() {
-        const input = document.getElementById('save-item')    
+        // adiciona o valor do input na variavel
+        const input = document.getElementById('save-item')
         const newItem = input.value
 
-        if(newItem.replace(/\s+/g, '') !== '') {
+        // se o valor for diferente de vazio o trecho a seguir é executado
+        // .replace(/\s+/g, '') remove os espaços para evitar que um 
+        // novo item seja adicionado somente com espaços
+        if (newItem.replace(/\s+/g, '') !== '') {
+            //aqui faz a checagem para não repetir um ID
             function checkId() {
-              let newId = 1
-              let idList = itemsList.map(item => item.id)
-    
-              while (idList.includes(newId)) {
-                newId += 1
-              }
-    
-              return newId
+                let newId = 1
+                let idList = itemsList.map(item => item.id)
+
+                while (idList.includes(newId)) {
+                    newId += 1
+                }
+
+                return newId
             }
-    
+
             const idValue = checkId()
-                
+
+            // adiciona o novo item
             setItemsList([
                 ...itemsList,
-                {id: idValue, item: newItem, checked: false}]
-            )
-            
+                { id: idValue, item: newItem, checked: false }
+            ])
+
+            // esvazia o input
             input.value = ''
+            // chama a função para desabilitar o botão adicionar 
             handleChangeEnable()
         }
     }
 
+    // Função que habilita e desabilita o botão adicionar do input
     function handleChangeEnable() {
         const inputItem = document.getElementById('save-item').value
-        
-        if(inputItem.replace(/\s+/g, '') !== '') {
-            setEnable(true)            
+
+        if (inputItem.replace(/\s+/g, '') !== '') {
+            setEnable(true)
         } else {
             setEnable(false)
         }
     }
-    
+
+    // altera o checked para true fazendo com que o item da lista 
+    // seja renderizado com um risco
     function check(id) {
-      setItemsList(
-        itemsList.map(item => {
-          if(item.id === id) {
-            return {...item, checked: true}
-          }
-          return item
-        })
-      )
+        setItemsList(
+            itemsList.map(item => {
+                if (item.id === id) {
+                    return { ...item, checked: true }
+                }
+                return item
+            })
+        )
     }
 
+    // remove o item da lista
     function remove(id) {
         setItemsList(itemsList.filter(item =>
             item.id !== id))
     }
 
+    // Boão para limpar lista
     function clearList() {
-      return (
-        <div className='button-line'>
-            <button onClick={handleClear} ><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon> Limpar</button>
-        </div>
-      )
+        return (
+            <div className='button-line'>
+                <button onClick={handleClear} ><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon> Limpar</button>
+            </div>
+        )
     }
 
+    //limpa lista
     function handleClear() {
-      setItemsList([])
+        setItemsList([])
     }
-    
+
     return (
-      <>
-        {clearList()}
-        <ul className="teste">{createItem()}</ul>                
-        {inputItem()}
-      </>
-    ) 
-  }
+        <>
+            {clearList()}
+            <ul>{createItem()}</ul>
+            {inputItem()}
+        </>
+    )
+}
